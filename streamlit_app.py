d48a0c909d5a21a23da6d0990483528ab1ad4f5e
jananikirubakkar <155720937+jananikirubakkar@users.noreply.github.com>
2024-01-05 14:21:24 +0530

    Updated dinner

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,9 @@ streamlit.title('My Kitchen')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal 🥣 ')
-streamlit.text('Kale, Spinach & Rocket Smoothie🥗 🥑')
+streamlit.text('Kale, Spinach & Rocket Smoothie🥗 ')
 streamlit.text('Hard-Boiled Free-Range Egg  🐔 🍞')
+streamlit.text('Avacado toast  🥑')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
