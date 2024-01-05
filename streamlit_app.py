ca072a9e7b7befc15ea99c69827646ec4a6fa566
jananikirubakkar <155720937+jananikirubakkar@users.noreply.github.com>
2024-01-05 14:19:21 +0530

    Updated dinner menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('Myself')
+streamlit.title('My Kitchen')
 
-streamlit.title('I had my breakfast')
-streamlit.header('Today is a good day')
-
-streamlit.text('working with code is interesting')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
